41cee0c22aca9d9460c9f02ea47fdab62008a9c2
alejo80805 <alejo80805@hotmail.com>
2015-07-11 17:03:18 -0500

    creando el modelo de posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+#-*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User)
+    title = models.CharField(max_length=150)
+    summary = models.TextField(default="")
+    body = models.TextField(default="")
+    url_image = models.URLField(null=True)
+    published_date = models.DateField()
+
+    def __unicode__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    post = models.ForeignKey(Post)
