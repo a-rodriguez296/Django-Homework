d532db8a820f795a1ba0c2bc32d919b726c81281
alejo80805 <alejo80805@hotmail.com>
2015-07-11 19:38:10 -0500

    comenzando a hacer home con la vista base ya implementada

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.views.generic import View
+from posts.models import Post
 
-# Create your views here.
+class HomeView(View):
+
+    def get(self, request):
+        posts = Post.objects.all().order_by('published_date')
+        context = {
+            'posts_list': posts
+        }
+        return render(request, 'posts/home.html', context)
--- wordplease/urls.py
+++ wordplease/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from posts.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^home$', HomeView.as_view(), name='posts_home'),
+
 ]
