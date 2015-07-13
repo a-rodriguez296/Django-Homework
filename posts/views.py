1bd95acaa7b9dc0c06341fdf9fe0cd2812db035f
alejo80805 <alejo80805@hotmail.com>
2015-07-13 15:56:05 -0500

    correccion alberto

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,7 @@ from posts.models import Post, Blog
 class HomeView(View):
 
     def get(self, request):
-        posts = Post.objects.all().order_by('published_date')
+        posts = Post.objects.all().order_by('-published_date')
         context = {
             'posts_list': posts
         }
@@ -16,8 +16,8 @@ class HomeView(View):
 
 class DetailView(View):
 
-    def get(self, request, pk):
-        possible_post = Post.objects.filter(pk=pk).select_related('blog')
+    def get(self, request, blog_id, post_identifier):
+        possible_post = Post.objects.filter(pk=post_identifier).select_related('blog')
         post = possible_post[0] if len(possible_post)>0 else None
 
         if post is not None:
@@ -25,12 +25,28 @@ class DetailView(View):
                 'post': post
             }
             return render(request, 'posts/detail.html', context)
+        else:
+            context = {
+                'post_id': post_identifier
+            }
+            return render(request, 'posts/empty_detail.html', context)
 
 
 class BlogsListView(View):
     def get(self, request):
         context = {
-            'blogs_list': Blog.objects.all()
+            'blogs_list': Blog.objects.all().order_by('name')
         }
         return render(request, 'posts/blogs_list.html', context)
 
+
+class BlogDetailView(View):
+
+    def get(self, request, pk):
+
+        possible_post = Post.objects.filter(blog_id=pk).order_by('-published_date')
+
+        context = {
+            'posts_list': possible_post,
+        }
+        return render(request, 'posts/blogs_detail.html', context)
--- wordplease/urls.py
+++ wordplease/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from posts.views import HomeView, DetailView, BlogsListView
+from posts.views import HomeView, DetailView, BlogsListView, BlogDetailView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^home$', HomeView.as_view(), name='posts_home'),
-    url(r'^posts/(?P<pk>[0-9]+)$', DetailView.as_view(), name='posts_detail'),
+    #url(r'^/$', HomeView.as_view(), name='posts_home'),
+    url(r'^blogs/(?P<blog_id>[0-9]+)/(?P<post_identifier>[0-9]+)$', DetailView.as_view(), name='posts_detail'),
 
     #Blogs
     url(r'^blogs$', BlogsListView.as_view(), name='blogs_list'),
+    url(r'^blogs/(?P<pk>[0-9]+)$', BlogDetailView.as_view(), name='blog_detail'),
 ]
+
