69e53354a38bb7ab5b6df6ec1dc8d855aefd3296
alejo80805 <alejo80805@hotmail.com>
2015-07-13 17:09:11 -0500

    se implemento el login

--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+#-*- coding: utf-8 -*-
+
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from users.forms import LoginForm
+from django.contrib.auth import authenticate, login as django_login
 
 
 class LoginView(View):
-
     def get(self, request):
-
         error_messages = []
         form = LoginForm()
         context = {
@@ -14,3 +15,30 @@ class LoginView(View):
             'login_form': form
         }
         return render(request, 'users/login.html', context)
+
+    def post(self, request):
+        error_messages = []
+
+        form = LoginForm(request.POST)
+
+        #con esto se valida que los datos cumplan con lo que se especifica en el modelo
+        if form.is_valid():
+
+            username = form.cleaned_data.get('usr')
+            password = form.cleaned_data.get('pwd')
+
+            user = authenticate(username=username, password=password)
+
+            if user is None:
+                error_messages.append('Nombre de usuario o contraseña incorrecta')
+            else:
+                if user.is_active:
+                    django_login(request, user)
+                    return redirect('posts_home')
+                else:
+                    error_messages.append('El usuario no está activo')
+            context = {
+                'errors': error_messages,
+                'login_form': form
+            }
+            return render(request, 'users/login.html', context)
